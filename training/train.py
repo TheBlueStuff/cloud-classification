5dcac10a194f7cf8f92961d4f933e7cdcabc3e74
Mario <mario.andonaire@pucp.edu.pe>
2022-10-24 15:38:49 -0500

    quick fix

--- training/train.py
+++ training/train.py
@@ -49,7 +49,7 @@ def read_args():
     parser.add_argument('--print-epochs', type=int, default=10)
     parser.add_argument('--print-times-per-epoch', type=int, default=20)
     parser.add_argument('--reduce-epochs', type=int, default=100)
-    parser.add_argument('--optimizer', type=int, default='Adam')
+    parser.add_argument('--optimizer', type=str, default='Adam')
     parser.add_argument('--early-stopping', type=bool, default=False)
     parser.add_argument('--n-cuda', type=int, default=0)
     return parser.parse_args()
